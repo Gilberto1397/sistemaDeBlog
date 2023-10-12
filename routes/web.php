478626e60623104a398f87fb35c3c7e6c019ebd9
Gilberto Azevedo Junior <gilberto-junior@outlook.com>
2023-10-12 10:33:26 -0300

    implementaLogout - Implementada rotina de logout na navBar

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::get('/', function () {
 
 Route::get('/login', [LoginController::class, 'create'])->name('login');
 Route::post('/login', [LoginController::class, 'store'])->name('login-post');
+Route::delete('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::get('/novo/usuario', [UserController::class, 'create'])->name('new-user-get');
 Route::post('/novo/usuario', [UserController::class, 'store'])->name('new-user-post');
